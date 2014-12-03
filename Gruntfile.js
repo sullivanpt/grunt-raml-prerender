d660ce7aa8c427c42f63feb2610f01e7f70b97d7
Patrick Sullivan <sullivanpt@acm.org>
2014-12-02 17:08:40 -0800

    fix: solve jshint camel case warning downstream

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     },
 
     // Configuration to be run (and then tested).
-    raml_prerender: {
+    ramlprerender: {
       default_options: {
         options: {
         },
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'raml_prerender', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'ramlprerender', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/ramlprerender_test.js
+++ test/ramlprerender_test.js
@@ -22,7 +22,7 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
-exports.raml_prerender = {
+exports.ramlprerender = {
   setUp: function(done) {
     // setup here if necessary
     done();
